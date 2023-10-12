ddeb800fed400d167d01c393a54ad7624f3088aa
Kalmax Santos <kalmax.santos.123@gmail.com>
2023-10-12 11:13:52 -0300

    Aula 03 - Implementção das Tasks (model, controller e repository); Codificação da senha no banco de dados; Filtro de controle para criação das tasks

---
+++ src/main/java/br/com/kalmaxsousa/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,51 @@
+package br.com.kalmaxsousa.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.kalmaxsousa.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        var authorization = request.getHeader("Authorization");
+        
+        authorization = authorization.replace("Basic ", "");
+
+        var authDecoded = new String(Base64.getDecoder().decode(authorization), "UTF-8");
+
+        var credentials = authDecoded.split(":");
+        var username = credentials[0];
+        var password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+
+        if(user == null){
+            response.sendError(401);
+        } else {
+            var passwordVerified = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if(passwordVerified.verified){
+                chain.doFilter(request, response);        
+            } else {
+                response.sendError(401);
+            }
+        }
+
+        
+    }
+    
+}
